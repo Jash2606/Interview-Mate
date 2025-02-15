355952f62ccca7da7dde4a1b78e595fd59cff8b1
Jash2606 <jash.23bcs10163@sst.scaler.com>
2025-02-16 02:04:58 +0530

    Created Meeting Page ..

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client "
+import LoaderUI from '@/components/LoaderUI';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useParams } from 'next/navigation';
+import React, { useState } from 'react';
+
 
 function MeetingPage() {
-  return (
-    <div>MeetingPage</div>
-  )
+  const{id} = useParams();
+  const{isLoaded} = useUser();
+  const isCallLoading = true;
+  const [isSetupComplete,setIsSetupComplete] = useState(false);
+  if(!isLoaded || isCallLoading) return <LoaderUI/>
+  return <StreamCall>
+      <StreamTheme>
+          {!isSetupComplete ? (
+              <MeetingSetup onSetupComplete={()=>setIsSetupComplete(true)}/>
+          ):(
+              <MeetingRoom/>
+          )}
+      </StreamTheme>
+  </StreamCall>
 }
-
 export default MeetingPage
