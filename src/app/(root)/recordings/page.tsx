21ebcbcda835db2472eef95fc3b178c56ecf5696
Jash2606 <jash.23bcs10163@sst.scaler.com>
2025-02-18 23:00:05 +0530

    Add useGetCalls hook to manage call data and integrate with RecordingPage..

--- src/app/(root)/recordings/page.tsx
+++ src/app/(root)/recordings/page.tsx
@@ -1,6 +1,9 @@
+import useGetCalls from '@/hooks/useGetCalls'
 import React from 'react'
 
 function RecordingPage() {
+  const { calls , isLoading} = useGetCalls();
+
   return (
     <div>RecordingPage</div>
   )
