282bbe7d322a6e9b88407b116cbb1dfe583fce38
Jash2606 <jash.23bcs10163@sst.scaler.com>
2025-02-18 23:42:31 +0530

    Add RecordingCard component and enhance RecordingsPage with recording fetching and display..

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
 import { clsx, type ClassValue } from "clsx"
+import { intervalToDuration } from "date-fns";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const calculateRecordingDuration = (startTime: string, endTime: string) => {
+  const start = new Date(startTime);
+  const end = new Date(endTime);
+
+  const duration = intervalToDuration({ start, end });
+
+  if (duration.hours && duration.hours > 0) {
+    // Without padding (padStart(2,"0")), the output might be "2:5" instead of "2:05"
+    return `${duration.hours}:${String(duration.minutes).padStart(2, "0")}:${String(duration.seconds).padStart(2, "0")}`;
+  }
+
+  if (duration.minutes && duration.minutes > 0) {
+    return `${duration.minutes}:${String(duration.seconds).padStart(2, "0")}`;
+  }
+
+  return `${duration.seconds} seconds`;
+};
